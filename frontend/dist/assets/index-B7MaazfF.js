2a640d9de68a5a548b84bb3ec7e4da8a9cf343fc
ekoxyprince <denniseinstien@gmail.com>
2025-11-30 17:30:18 +0100

    chore:

---
+++ frontend/dist/assets/index-B7MaazfF.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-Dt5abts3.js";var o=a;export{o as default};
--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("http://194.163.168.60:3000", {
   autoConnect: false,
   auth: {
     token: null,
