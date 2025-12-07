e59043a2dc12f4fb3a408bdc9ca7acd481a7f76f
ekoxyprince <denniseinstien@gmail.com>
2025-12-07 18:25:47 +0100

    chore

--- backend/services/user/user-leaderboards.js
+++ backend/services/user/user-leaderboards.js
@@ -2,7 +2,9 @@ import User from "../../database/models/user.js";
 
 export default async function () {
   try {
-    const leaderboard = await User.find().sort("-tournamentsWon").limit(20);
+    const leaderboard = await User.find({ role: "user" })
+      .sort("-tournamentsWon")
+      .limit(20);
     return leaderboard;
   } catch (error) {
     throw error;
